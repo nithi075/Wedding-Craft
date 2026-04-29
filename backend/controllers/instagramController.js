862e02acc89a4613555c52b8e1cfaf3923e4a4a0
Sam Nithish <nithishraj8055@gmail.com>
2026-04-29 21:40:04 +0530

    Update instagramController.js

--- backend/controllers/instagramController.js
+++ backend/controllers/instagramController.js
@@ -2,35 +2,59 @@ const Instagram = require("../models/Instagram");
 
 const addInstagram = async (req, res) => {
   try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Image is required"
+      });
+    }
+
     const data = new Instagram({
       title: req.body.title,
       postLink: req.body.postLink,
-      imageUrl: `http://localhost:5000/uploads/${req.file.filename}`
+      imageUrl: req.file.path
     });
 
     await data.save();
+
     res.json(data);
 
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
 
 const getInstagram = async (req, res) => {
   try {
-    const data = await Instagram.find();
+    const data =
+      await Instagram.find();
+
     res.json(data);
+
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 };
 
 const deleteInstagram = async (req, res) => {
   try {
-    await Instagram.findByIdAndDelete(req.params.id);
-    res.json({ message: "Deleted Successfully" });
+    await Instagram.findByIdAndDelete(
+      req.params.id
+    );
+
+    res.json({
+      message:
+        "Deleted Successfully"
+    });
+
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      error:
+        error.message
+    });
   }
 };
 
